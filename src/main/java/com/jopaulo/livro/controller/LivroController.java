92302250ece7497978e8a0c7e76c870b76bc79f8
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-01-18 20:00:01 -0300

    CORS: Salvando livros atraves da Aplicaçao web para a REST API

--- src/main/java/com/jopaulo/livro/controller/LivroController.java
+++ src/main/java/com/jopaulo/livro/controller/LivroController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.jopaulo.livros.repository.LivroRepository;
 
 @RestController
 @RequestMapping("/api/livros")
+@CrossOrigin("http://localhost:4200")
 public class LivroController {
 
 	@Autowired
